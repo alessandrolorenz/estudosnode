5f88a91f107aee9c1a4fc5ece182c5653b6a064f
alessandrocunhalorenz <alessandro.lorenz@gmail.com>
2019-08-19 09:20:52 -0300

    Rotas consign e ajustes

--- Noticias/app.js
+++ Noticias/app.js
@@ -2,14 +2,13 @@ var app = require('./config/server')
 
 var port = 3000;
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+// var rotaNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+// var rotaHome = require('./app/routes/home');
+// rotaHome(app);
 
-var rotaNoticias= require('./app/routes/noticias');
-rotaNoticias(app);
+// var rotaInclusaoNoticias= require('./app/routes/formulario_inclusao');
+// rotaInclusaoNoticias(app);
 
 app.listen(port, function(){
     console.log("Servidor rodando com Express na porta: " + port);
