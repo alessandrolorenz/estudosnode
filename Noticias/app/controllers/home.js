efe9b1e8a9266cb63a73864eddb9a38f37fe7a30
alessandrocunhalorenz <alessandro.lorenz@gmail.com>
2019-08-27 09:17:37 -0300

    Adequação da index com as 5 ultimas noticias

--- Noticias/app/controllers/home.js
+++ Noticias/app/controllers/home.js
@@ -1,3 +1,16 @@
 module.exports.index = function(app, req, res) {
-    res.render("home/index");
+
+    var connection = app.config.dbConnection();
+    var noticiasModel = new app.app.models.NoticiasDAO(connection); //uma instancia de NoticiasDAO
+
+    // depois da consulta é necessario renderizar a consulta
+    // - function que sera usada como callback para a function query(que retorna erro ou um resultado)
+    
+    noticiasModel.get5UltimasNoticias(function(error, result){
+
+        res.render("home/index", {noticias : result});
+    });
+
+
+    
 }
